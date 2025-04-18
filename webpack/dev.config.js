e87eb65b0fea110d1cd632759aa0e4882222bc35
Ruslan Oyun <careerRuslanOyun@yandex.ru>
2025-04-19 03:02:17 +0600

    add headers in config

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -14,7 +14,9 @@ module.exports = (env) =>
       hot: true,
       historyApiFallback: true,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
       },
     },
   });
